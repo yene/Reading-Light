a1769189a492a7b5bb064e3d66ed601919304772
yene <yene@cocoadojo.com>
2015-06-24 22:45:31 +0200

    inital commit

---
+++ Reading Light/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reading Light
+//
+//  Created by Yannick Weiss on 24/06/15.
+//  Copyright (c) 2015 Yannick Weiss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
